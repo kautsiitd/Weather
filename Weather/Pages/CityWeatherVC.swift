d9d449e9a0f3ef70a7c6f886fcf7f4cc77eb0eb4
Kautsya Kanu <kautsya.kanu@saavn.com>
2021-02-22 05:06:12 +0530

    1. Added BaseViewController for Loader, events etc handling

---
+++ Weather/Pages/CityWeatherVC.swift
@@ -0,0 +1,11 @@
+//
+//  CityWeatherVC.swift
+//  Weather
+//
+//  Created by Kautsya Kanu on 21/02/21.
+//
+
+import UIKit
+class CityWeatherVC: BaseViewController {
+    
+}
--- Weather/Pages/SearchPageVC.swift
+++ Weather/Pages/SearchPageVC.swift
@@ -6,6 +6,15 @@
 //
 
 import UIKit
-class SearchPageVC: UIViewController {
-    
+class SearchPageVC: BaseViewController {
+    //MARK:- Elements
+    @IBOutlet private var searchBar: UISearchBar!
+    @IBOutlet private var tableView: UITableView!
+    //MARK:- Properties
+}
+
+extension SearchPageVC: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
 }
