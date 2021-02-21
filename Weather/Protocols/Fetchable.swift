07cfc5d8fe383f7c2f8ac8ea7016450b229d8b62
Kautsya Kanu <kautsya.kanu@saavn.com>
2021-02-22 03:49:18 +0530

    Added 1. ApiRespondable: Something that can respond to ApiResponse parsed from server 2. Fetchable: Something that can be fetched from server Protocols

---
+++ Weather/Protocols/Fetchable.swift
@@ -0,0 +1,16 @@
+//
+//  Fetchable.swift
+//  Weather
+//
+//  Created by Kautsya Kanu on 21/02/21.
+//
+
+import Foundation
+protocol Fetchable: ApiRespondable {
+    var status: ApiStatus { get }
+    var apiEndPoint: String { get }
+    var params: [String: AnyHashable] { get }
+    func makeGetRequest()
+    func shouldParse(for params: [String: AnyHashable]) -> Bool
+    func parse(_ data: Data, for params: [String: AnyHashable]) throws
+}
