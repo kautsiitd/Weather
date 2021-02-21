df36f172ddd59427a2c1c7bcb6914c0ad6c567a9
Kautsya Kanu <kautsya.kanu@saavn.com>
2021-02-22 04:24:54 +0530

    Added Search and LikedPageVCs and made connection in StoryBoard

---
+++ Weather/Pages/LikedPageVC.swift
@@ -0,0 +1,11 @@
+//
+//  LikedPageVC.swift
+//  Weather
+//
+//  Created by Kautsya Kanu on 22/02/21.
+//
+
+import UIKit
+class LikedPageVC: UIViewController {
+    
+}
---
+++ Weather/Pages/SearchPageVC.swift
@@ -0,0 +1,11 @@
+//
+//  SearchPageVC.swift
+//  Weather
+//
+//  Created by Kautsya Kanu on 22/02/21.
+//
+
+import UIKit
+class SearchPageVC: UIViewController {
+    
+}
