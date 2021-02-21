3c0bcf803e1e4412b3ec72c40e617a165d404eeb
Kautsya Kanu <kautsya.kanu@saavn.com>
2021-02-22 03:52:22 +0530

    Added BaseApiModal to Fetch response from server

---
+++ Weather/Networking/BaseApiModal.swift
@@ -0,0 +1,36 @@
+//
+//  BaseApiModal.swift
+//  Weather
+//
+//  Created by Kautsya Kanu on 20/02/21.
+//
+
+import UIKit
+class BaseApiModal: NSObject, Fetchable {
+    weak var delegate: ApiRespondable?
+    private var latestParams: [String: AnyHashable] = [:]
+    
+    //MARK:- Fetchable
+    var status: ApiStatus = .unknown
+    var apiEndPoint: String { "" }
+    var params: [String: AnyHashable] { [:] }
+    
+    func makeGetRequest() {
+        status = .inProgress
+        latestParams = params
+        do { try ApiManager.shared.getRequest(self) }
+        catch let error { delegate?.didFail(with: ApiError.custom(error), for: params) }
+    }
+    
+    func shouldParse(for params: [String : AnyHashable]) -> Bool {
+        return params == latestParams
+    }
+    func parse(_ data: Data, for params: [String : AnyHashable]) throws {}
+    func didFetchSuccessfully(for params: [String : AnyHashable]) {}
+    func didFail(with error: BaseError, for params: [String : AnyHashable]) {}
+}
+
+//MARK:- Helpers
+enum ApiStatus {
+    case inProgress, success, error(with: BaseError), contentOver, unknown
+}
