83bc171b5d2784dee91b6f5cfe2e4e3566687b4a
Kautsya Kanu <kautsya.kanu@saavn.com>
2021-02-22 13:02:35 +0530

    1. Minor update in DailyForecastApi and CurrentWeatherApi 2. Added support of coord fetch in CurrentWeatherApi

--- Weather/Apis/Current/CurrentWeatherApi.swift
+++ Weather/Apis/Current/CurrentWeatherApi.swift
@@ -8,14 +8,21 @@
 import Foundation
 class CurrentWeatherApi: BaseApiModal {
     //MARK:- Properties
-    private var query: String = ""
+    var query: String?
+    var location: Coordinates?
     private var units: MeasurementUnit = .standard
     private var language: Language = .english
     var cityWeather: CurrentWeather?
     //MARK:- Fetchable
-    override var apiEndPoint: String { "weather" }
+    override var apiEndPoint: String { "data/2.5/weather" }
     override var params: [String : AnyHashable] {
-        ["q": query, "units": units.rawValue, "lang": language.code]
+        var dict: [String: AnyHashable] = ["units": units.rawValue, "lang": language.code]
+        if let query = query { dict["q"] = query }
+        else if let location = location {
+            dict["lat"] = location.lat
+            dict["lon"] = location.lon }
+        else { dict["q"] = "Globe" }
+        return dict
     }
     override func parse(_ data: Data, for params: [String : AnyHashable]) throws {
         let decoder = JSONDecoder()
