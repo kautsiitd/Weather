560dd2177945f50c0c43b171e5d9489fbb6fbf41
Kautsya Kanu <kautsya.kanu@saavn.com>
2021-02-24 01:31:57 +0530

    1. Fixed Minor parsing issue in Wind 2. Added functionality of lat lon in ForecastApi

--- Weather/Apis/Forecast/ForecastApi.swift
+++ Weather/Apis/Forecast/ForecastApi.swift
@@ -11,13 +11,20 @@ class ForecastApi: BaseApiModal {
     var query: String?
     var location: Coordinates?
     private var numberOfTimeStamps: Int = 8
-    private var units: MeasurementUnit = .standard
+    private var units: MeasurementUnit = .metric
     private var language: Language = .english
     var forecast: ForecastWeather?
     //MARK:- Fetchable
     override var apiEndPoint: String { "data/2.5/forecast" }
     override var params: [String : AnyHashable] {
-        ["q": query, "cnt": numberOfTimeStamps, "units": units, "lang": language.code]
+        var dict: [String: AnyHashable] = ["cnt": numberOfTimeStamps,
+                                           "units": units.rawValue, "lang": language.code]
+        if let query = query { dict["q"] = query }
+        else if let location = location {
+            dict["lat"] = location.lat
+            dict["lon"] = location.lon }
+        else { dict["q"] = "Globe" }
+        return dict
     }
     override func parse(_ data: Data, for params: [String : AnyHashable]) throws {
         let decoder = JSONDecoder()
